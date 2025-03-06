6944aba3722ec21276665a97cd43c7c6ce7de6ee
astro-pavan <pt426@cam.ac.uk>
2025-03-06 12:26:42 +0000

    Added generating code from layer class

--- planet/hydrosphere.py
+++ planet/hydrosphere.py
@@ -1,4 +1,16 @@
-class hydrosphere:
+from layer import layer, M_earth, R_earth
+from EOS.H2O import eos_water
 
-    def __init__(self):
-        pass
+class hydrosphere(layer):
+
+    def __init__(self, m_top, m_bottom, r_start, P_start, T_start, integrate_down=True, temp_profile='adiabat'):
+
+        eos_h2o = eos_water()
+        eos_h2o.make_interpolators()
+        
+        super().__init__(m_top, m_bottom, r_start, P_start, T_start, eos_h2o, integrate_down, temp_profile)
+
+
+if __name__ == '__main__':
+
+    test = hydrosphere(M_earth * 5, M_earth * 4, R_earth * 2, 1e5, 300)
