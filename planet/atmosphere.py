27320de9a4e485e302dcfabb44929209c4e9f598
astro-pavan <pt426@cam.ac.uk>
2025-03-06 15:04:21 +0000

    Uses the layer code to generate an atmosphere

--- planet/atmosphere.py
+++ planet/atmosphere.py
@@ -1,4 +1,29 @@
-class atmosphere:
+from layer import layer, M_earth, R_earth
+from EOS.H2O import eos_water
 
-    def __init__(self):
-        pass
+class atmo(layer):
+
+    def __init__(self, m_bottom, r_bottom, P_surface, T_surface, atmosphere_type, P_min=1):
+        
+        self.eos = None
+
+        if atmosphere_type == 'HHe':
+            pass
+        elif atmosphere_type == 'H2O':
+            self.eos = eos_water()
+        elif atmosphere_type == 'CO2':
+            pass
+        elif atmosphere_type == 'N2':
+            pass
+
+        self.eos.make_interpolators()
+
+        super().__init__(m_bottom * 1.5, m_bottom, r_bottom, P_surface, T_surface, self.eos, integrate_down=False, temp_profile='isothermal')
+
+        P_mask = self.P > P_min
+
+        self.m = self.m[P_mask]
+        self.r = self.r[P_mask]
+        self.P = self.P[P_mask]
+        self.T = self.T[P_mask]
+        self.rho = self.rho[P_mask]
