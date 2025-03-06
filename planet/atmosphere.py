928102ded1222b29237baf0602242847837cb5cc
astro-pavan <pt426@cam.ac.uk>
2025-03-06 17:05:24 +0000

    Added code that can run the AGNI atmosphere code

--- planet/atmosphere.py
+++ planet/atmosphere.py
@@ -1,5 +1,10 @@
 from layer import layer, M_earth, R_earth
 from EOS.H2O import eos_water
+from scipy.io import netcdf
+
+import os
+
+AGNI_path = '/home/pt426/AGNI'
 
 class atmo(layer):
 
@@ -17,8 +22,9 @@ class atmo(layer):
             pass
 
         self.eos.make_interpolators()
+        self.max_mass_step = None
 
-        super().__init__(m_bottom * 1.5, m_bottom, r_bottom, P_surface, T_surface, self.eos, integrate_down=False, temp_profile='isothermal')
+        super().__init__(m_bottom * 1.1, m_bottom, r_bottom, P_surface, T_surface, self.eos, integrate_down=False, temp_profile='isothermal')
 
         P_mask = self.P > P_min
 
@@ -27,3 +33,51 @@ class atmo(layer):
         self.P = self.P[P_mask]
         self.T = self.T[P_mask]
         self.rho = self.rho[P_mask]
+
+
+    def run_AGNI(self):
+
+        wd = os.getcwd()
+        config_file_path = f'{AGNI_path}/res/config/default.toml'
+        config_file_path_new = f'{AGNI_path}/res/config/pl2.toml'
+
+        # print(self.m / M_earth)
+        # print(self.r / R_earth)
+        # print(self.P / 1e5)
+
+        P_surface = self.P[0] / 1e5
+        T_surface = self.T[0]
+
+        config_file_modifications = {
+            5 : 'title = "pl2"',
+            55 : '    solution_type   = 3                         # Solution type (see wiki).',
+            30 : f'    p_surf          = {P_surface:.2f}                     # Total surface pressure [bar].',
+            8 : f'    tmp_surf        = {T_surface:.1f}            # Surface temperature [kelvin].'
+        }
+
+        try:
+            # Read the file into a list of lines
+            with open(config_file_path, 'r') as file:
+                lines = file.readlines()
+            
+            # Apply modifications
+            for line_number, new_content in config_file_modifications.items():
+                if 1 <= line_number <= len(lines):
+                    lines[line_number - 1] = new_content + '\n'
+                else:
+                    print(f"Line {line_number} is out of range. Skipping.")
+            
+            # Write the modified lines back to the file
+            with open(config_file_path_new, 'w') as file:
+                file.writelines(lines)
+
+            print("File modified successfully.")
+        
+        except FileNotFoundError:
+            print(f"Error: The file at '{config_file_path}' was not found.")
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+
+        # os.chdir(AGNI_path)
+        # os.system('./agni.jl res/config/pl2.toml')
+        # os.chdir(wd)
