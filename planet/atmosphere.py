b954c7fcb79bad7270bed591c7583308932348e0
astro-pavan <pt426@cam.ac.uk>
2025-03-07 14:19:50 +0000

    Added code that runs the atmosphere mondel with planet conditions.

--- planet/atmosphere.py
+++ planet/atmosphere.py
@@ -1,4 +1,4 @@
-from layer import layer, M_earth, R_earth
+from layer import layer, M_earth, R_earth, G
 from EOS.H2O import eos_water
 from scipy.io import netcdf
 
@@ -6,9 +6,25 @@ import os
 
 AGNI_path = '/home/pt426/AGNI'
 
+spectral_types = {
+    'M8' : 'trappist-1.txt',
+    'M4.5': 'gj1214.txt',
+    'M4' : 'gj1132.txt',
+    'M3.5' : 'gj849.txt',
+    'M3' : 'l-98-59.txt',
+    'M2' : 'gj179.txt',
+    'K6' : 'hd85512.txt',
+    'K2' : 'eps-eri.txt',
+    'K1' : 'hd97658.txt',
+    'G2' : 'sun.txt',
+    'G0' : 'HIP67522.txt'
+}
+
+solar_constant = 1360 # W / m^2
+
 class atmo(layer):
 
-    def __init__(self, m_bottom, r_bottom, P_surface, T_surface, atmosphere_type, P_min=1):
+    def __init__(self, m_bottom, r_bottom, P_surface, T_surface, atmosphere_type, instellation=1, spectral_type='G2', P_min=1):
         
         self.eos = None
 
@@ -34,6 +50,9 @@ class atmo(layer):
         self.T = self.T[P_mask]
         self.rho = self.rho[P_mask]
 
+        self.instellation = instellation
+        self.host_star_spectral_type = spectral_type
+
 
     def run_AGNI(self):
 
@@ -41,18 +60,24 @@ class atmo(layer):
         config_file_path = f'{AGNI_path}/res/config/default.toml'
         config_file_path_new = f'{AGNI_path}/res/config/pl2.toml'
 
-        # print(self.m / M_earth)
-        # print(self.r / R_earth)
-        # print(self.P / 1e5)
-
         P_surface = self.P[0] / 1e5
         T_surface = self.T[0]
+        r_surface = self.r[0]
+        m_surface = self.m[0]
+
+        g_surface = (G * m_surface) / (r_surface ** 2)
 
         config_file_modifications = {
             5 : 'title = "pl2"',
             55 : '    solution_type   = 3                         # Solution type (see wiki).',
             30 : f'    p_surf          = {P_surface:.2f}                     # Total surface pressure [bar].',
-            8 : f'    tmp_surf        = {T_surface:.1f}            # Surface temperature [kelvin].'
+            8 : f'    tmp_surf        = {T_surface:.1f}            # Surface temperature [kelvin].',
+            15 : f'    radius          = {r_surface:.3e}            # Planet radius at the surface [m].',
+            16 : f'    gravity         = {g_surface:.2e}              # Gravitational acceleration at the surface [m s-2]',
+            9 : f'    instellation    = {self.instellation * solar_constant:.1f}           # Stellar flux at planet\'s orbital distance [W m-2].',
+            26 : f'    input_star      = "res/stellar_spectra/{spectral_types[self.host_star_spectral_type]}"              # Path to stellar spectrum.',
+            #58 : f'    initial_state   = ["iso", "300"]     # Ordered list of requests describing the initial state of the atmosphere (see wiki).',
+            56 : f'    solvers         = ["newton"]                        # Ordered list of solvers to apply (see wiki).'
         }
 
         try:
@@ -78,6 +103,6 @@ class atmo(layer):
         except Exception as e:
             print(f"An unexpected error occurred: {e}")
 
-        # os.chdir(AGNI_path)
-        # os.system('./agni.jl res/config/pl2.toml')
-        # os.chdir(wd)
+        os.chdir(AGNI_path)
+        os.system('./agni.jl res/config/pl2.toml')
+        os.chdir(wd)
