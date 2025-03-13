94abacdb6d5b0d94ee2020191bf88b6706a30db7
Pavan Tanna <117482628+astro-pavan@users.noreply.github.com>
2025-03-13 17:35:56 +0000

    Started planet creation code

--- planet/planet.py
+++ planet/planet.py
@@ -9,15 +9,60 @@ R_earth = 6371000 # m
 
 class planet:
 
-    def __init__(self, mass, radius, water_mass_fraction, P_surface, T_surface, atmosphere_type, star_spectral_type='G2', instellation=1):
+    def __init__(self):
 
-        water_mass = water_mass_fraction * mass
+        self.mass = None
+        self.radius = None
+        
+        self.water_mass_fraction = None
+        self.hydrogen_mass_fraction = None
 
-        print('Generating hydrosphere...')
-        self.hydrosphere = hydrosphere(mass, mass - water_mass, radius, P_surface, T_surface)
+        self.instellation = None
+        self.host_star_spectral_type = None
 
-        print('Generating atmosphere...')
-        self.atmosphere = atmo(mass, radius, P_surface, T_surface, 'H2O', instellation, star_spectral_type)
+        self.P_surface = None
+        self.T_surface = None
+
+        self.find_R, self.find_M, self.find_x_H2O, self.find_x_H = False, False, False, False
+        self.find_star_properties, self.find_surface_conditions = False, False
+
+    def set_bulk_properties(self, mass=None, radius=None, water_mass_fraction=None, hydrogen_mass_fraction=None):
+
+        self.mass = mass
+        self.radius = radius
+        self.water_mass_fraction = water_mass_fraction
+        self.hydrogen_mass_fraction = hydrogen_mass_fraction
+
+        self.find_R = radius is None
+        self.find_M = mass is None
+        self.find_x_H2O = water_mass_fraction is None
+        self.find_x_H = hydrogen_mass_fraction is None
+
+    def set_surface_conditions(self, P_surface, T_surface):
+
+        self.P_surface = P_surface
+        self.T_surface = T_surface
+
+        self.find_star_properties = True
+
+    def set_host_star(self, instellation, spectral_type):
+
+        self.instellation = instellation
+        self.host_star_spectral_type = spectral_type
+
+        self.find_surface_conditions = True
+
+    def set_chemical_composition(self):
+        pass
+
+    def generate_planet(self):
+        pass
+    
+    # print('Generating hydrosphere...')
+    # self.hydrosphere = hydrosphere(mass, mass - water_mass, radius, P_surface, T_surface)
+
+    # print('Generating atmosphere...')
+    # self.atmosphere = atmo(mass, radius, P_surface, T_surface, 'H2O', instellation, star_spectral_type)
 
 
 if __name__ == '__main__':
