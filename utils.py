ad0cc8f39c9432af7bceb22db1f1738614e2610d
astro-pavan <pt426@cam.ac.uk>
2025-03-14 14:39:02 +0000

    File modification function added

--- utils.py
+++ utils.py
@@ -1,41 +1,26 @@
-import numpy as np
 
-def make_into_pair_array(arr1, arr2):
-    # turns two multidimensional numpy arrays into a form that can be used with the scipy interpolator
 
-    arr1, arr2 = np.nan_to_num(arr1), np.nan_to_num(arr2)
+def modify_file_by_lines(file_path, new_file_path, modification_dict):
 
-    if type(arr1) is np.ndarray and type(arr2) is np.ndarray:
+    try:
+            # Read the file into a list of lines
+            with open(file_path, 'r') as file:
+                lines = file.readlines()
+            
+            # Apply modifications
+            for line_number, new_content in modification_dict.items():
+                if 1 <= line_number <= len(lines):
+                    lines[line_number - 1] = new_content + '\n'
+                else:
+                    print(f"Line {line_number} is out of range. Skipping.")
+            
+            # Write the modified lines back to the file
+            with open(new_file_path, 'w') as file:
+                file.writelines(lines)
 
-        if arr1.ndim == 0:
-            return np.array([arr1[()], arr2[0]])
-        if arr2.ndim == 0:
-            return np.array([arr1[0], arr2[()]])
-
-        try:
-            assert np.all(arr1.shape == arr2.shape)
-        except AssertionError:
-            print(f'arr1 = {arr1} \n arr1 shape = {arr1.shape}')
-            print(f'arr2 = {arr2} \n arr2 shape = {arr2.shape}')
-            assert np.all(arr1.shape == arr2.shape)
-
-        assert arr1.ndim == 1 or arr1.ndim == 2
-
-        arr = np.array([arr1, arr2])
-
-        if arr1.ndim == 1:
-            return np.transpose(arr, axes=(1, 0))
-        elif arr1.ndim == 2:
-            return np.transpose(arr, axes=(1, 2, 0))
-
-    else:
-
-        if type(arr1) is np.ndarray:
-            if arr1.ndim == 1:
-                arr1 = arr1[0]
-
-        if type(arr2) is np.ndarray:
-            if arr2.ndim == 1:
-                arr2 = arr2[0]
-
-        return np.array([arr1, arr2])
+            print("File modified successfully.")
+        
+    except FileNotFoundError:
+        print(f"Error: The file at '{file_path}' was not found.")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
