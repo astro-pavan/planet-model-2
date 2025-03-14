d38fe90fa023c54912084cc8ead803be632160f9
astro-pavan <pt426@cam.ac.uk>
2025-03-14 14:16:43 +0000

    Moved a utility function into file.

--- eos_water.py
+++ eos_water.py
@@ -4,7 +4,45 @@ from scipy.interpolate import interp1d, RegularGridInterpolator, CubicSpline
 from contourpy import contour_generator
 import matplotlib.pyplot as plt
 
-from utils import make_into_pair_array
+def make_into_pair_array(arr1, arr2):
+    # turns two multidimensional numpy arrays into a form that can be used with the scipy interpolator
+
+    arr1, arr2 = np.nan_to_num(arr1), np.nan_to_num(arr2)
+
+    if type(arr1) is np.ndarray and type(arr2) is np.ndarray:
+
+        if arr1.ndim == 0:
+            return np.array([arr1[()], arr2[0]])
+        if arr2.ndim == 0:
+            return np.array([arr1[0], arr2[()]])
+
+        try:
+            assert np.all(arr1.shape == arr2.shape)
+        except AssertionError:
+            print(f'arr1 = {arr1} \n arr1 shape = {arr1.shape}')
+            print(f'arr2 = {arr2} \n arr2 shape = {arr2.shape}')
+            assert np.all(arr1.shape == arr2.shape)
+
+        assert arr1.ndim == 1 or arr1.ndim == 2
+
+        arr = np.array([arr1, arr2])
+
+        if arr1.ndim == 1:
+            return np.transpose(arr, axes=(1, 0))
+        elif arr1.ndim == 2:
+            return np.transpose(arr, axes=(1, 2, 0))
+
+    else:
+
+        if type(arr1) is np.ndarray:
+            if arr1.ndim == 1:
+                arr1 = arr1[0]
+
+        if type(arr2) is np.ndarray:
+            if arr2.ndim == 1:
+                arr2 = arr2[0]
+
+        return np.array([arr1, arr2])
 
 # Define column names based on the table header
 columns = [
