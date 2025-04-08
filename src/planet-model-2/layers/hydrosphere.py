9f6d7ef0bb117ae8bcc343de6dc9e2038b1bb31e
astro-pavan <pt426@cam.ac.uk>
2025-04-08 14:29:14 +0100

    Building code that can use phreeqc

--- src/planet-model-2/layers/hydrosphere.py
+++ src/planet-model-2/layers/hydrosphere.py
@@ -1,7 +1,9 @@
-from layers.layer import layer, M_Earth, R_EARTH
+from layers.layer import layer
 from EOS.H2O import eos_water
 from utils import modify_file_by_lines
+from constants import M_EARTH, R_EARTH, EARTH_ATM, ABSOLUTE_ZERO
 
+import numpy as np
 import os
 
 PHREEQC_path = '/data/pt426/phreeqc/bin'
@@ -10,7 +12,10 @@ class hydrosphere(layer):
     
     def calculate_CO2(self):
 
-        input_file_modifications = {}
+        input_file_modifications = {
+            4 : f'temp        60.0        # Temperature in degrees Celsius [1, 2]',
+            5 : f'pressure    1.5         # Pressure in atmospheres [2]'
+        }
 
         input_file_path = 'templates/phreeqc_co2_equilibrium_input_template.txt'
         input_file_path_new = f'{PHREEQC_path}/input'
@@ -23,9 +28,27 @@ class hydrosphere(layer):
         os.chdir(wd)
 
 
+def equilbriate_CO2(P, T, pH_initial, C_dissolved_initial, P_CO2_initial):
+    pass
 
 
-if __name__ == '__main__':
+def phreeqc_CO2_equilibrium_phase(P, T, pH, C_dissolved, P_CO2):
+    
+    input_template_file_path = 'templates/phreeqc_co2_equilibrium_input_template.txt'
+    input_file_path_new = f'{PHREEQC_path}/input'
+    output_file_path = f'{PHREEQC_path}/input.out'
+    
+    input_file_modifications = {
+            4 : f'    temp        {T + ABSOLUTE_ZERO:.1f}        # Temperature in degrees Celsius [1, 2]',
+            5 : f'    pressure    {P / EARTH_ATM:.2f}         # Pressure in atmospheres [2]',
+            6 : f'    pH          {pH:.1f}         # Initial pH',
+            8 : f'    C           {C_dissolved:.2f}         # Total dissolved carbon [4]',
+            11 : f'    CO2(g)      {np.log10(P_CO2 / EARTH_ATM)}        10.0    # partial pressure in log(atm) and number of moles of CO2'
+        }
 
-    test = hydrosphere(M_Earth * 5, M_Earth * 4, R_EARTH * 2, 1e5, 300)
-    test.calculate_CO2()
+    modify_file_by_lines(input_template_file_path, input_file_path_new, input_file_modifications)
+
+    wd = os.getcwd()
+    os.chdir(PHREEQC_path)
+    os.system('./phreeqc input')
+    os.chdir(wd)
