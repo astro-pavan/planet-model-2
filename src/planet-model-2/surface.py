9df0f0286041031a7fa5956166ca18c5b4c57bcf
astro-pavan <pt426@cam.ac.uk>
2025-04-10 17:55:11 +0100

    Better equilbrium phase calculation method

--- src/planet-model-2/surface.py
+++ src/planet-model-2/surface.py
@@ -4,8 +4,9 @@ from constants import ABSOLUTE_ZERO, EARTH_ATM
 from utils import modify_file_by_lines
 
 import numpy as np
-import os
 import pandas as pd
+import os
+import subprocess
 
 PHREEQC_path = '/data/pt426/phreeqc/bin'
 
@@ -18,6 +19,25 @@ class surface:
 
         self.P = self.atmosphere.P[-1]
         self.T = self.atmosphere.T[-1]
+
+        self.gas_ocean_interactions()
+        self.atmosphere.run_AGNI(T_iso=self.T, high_spectral_res=True)
+
+        self.gas_ocean_interactions()
+        self.atmosphere.run_AGNI(T_iso=self.T, high_spectral_res=True)
+
+    def gas_ocean_interactions(self):
+
+        self.H2O_evaporation()
+
+        n, d_CO2 = 0, 1
+
+        while np.abs(d_CO2) > 0.01:
+            d_CO2 = self.phreeqc_equilibrium_phase(keep_atmosphere_constant=True)
+            n += 1
+            print(f"\rCALCULATING HYDROSPHERE CO2 CONTENT (STEPS: {n})", end='', flush=True)
+        
+        print('')
 
     def H2O_evaporation(self):
 
@@ -52,7 +72,11 @@ class surface:
         mol_atm = atmosphere_column_density / mmw_atmosphere
         mol_CO2 = mol_atm * self.atmosphere.x_gas['CO2'][0]
 
-        scale_factor = 1 / mol_CO2
+        if mol_CO2 == 0:
+            scale_factor = 10000
+        else:
+            scale_factor = 1 / mol_CO2
+
         m_water = hydrosphere_column_density * scale_factor
 
         P_CO2 = self.P * self.atmosphere.x_gas['CO2'][0]
@@ -61,19 +85,19 @@ class surface:
                 4 : f'    temp        {self.T + ABSOLUTE_ZERO:.1f}        # Temperature in degrees Celsius',
                 5 : f'    pressure    {self.P / EARTH_ATM:.2f}         # Pressure in atmospheres',
                 6 : f'    pH          {self.hydrosphere.pH:.1f}         # Initial pH',
-                8 : f'    Ca          {self.hydrosphere.molarity["Ca"]:.2f}         # Calcium concentration',
-                9 : f'    Na          {self.hydrosphere.molarity["Na"]:.2f}         # Sodium concentration',
-                10 : f'    Cl          {self.hydrosphere.molarity["Cl"]:.2f}         # Chloride concentration',
-                11 : f'    C           {self.hydrosphere.molarity["C"]:.2f}         # Total dissolved carbon',
+                8 : f'    Ca          {self.hydrosphere.molarity["Ca"] * 1000:.6f}         # Calcium concentration',
+                9 : f'    Na          {self.hydrosphere.molarity["Na"] * 1000:.6f}         # Sodium concentration',
+                10 : f'    Cl          {self.hydrosphere.molarity["Cl"] * 1000:.6f}         # Chloride concentration',
+                11 : f'    C           {self.hydrosphere.molarity["C"] * 1000:.6f}         # Total dissolved carbon',
                 12 : f'    water       {m_water:.2f}         # mass of water in kg',
-                15 : f'    CO2(g)      {np.log10(P_CO2 / EARTH_ATM)}        {1:.2f}    # partial pressure in log(atm) and number of moles of CO2',
+                15 : f'    CO2(g)      {np.max([np.log10(P_CO2 / EARTH_ATM), -10])}        {mol_CO2 * scale_factor:.2f}    # partial pressure in log(atm) and number of moles of CO2',
             }
 
         modify_file_by_lines(input_template_file_path, input_file_path_new, input_file_modifications)
 
         wd = os.getcwd()
         os.chdir(PHREEQC_path)
-        os.system('./phreeqc input')
+        subprocess.run(['./phreeqc', 'input'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         os.chdir(wd)
 
         solution_df = pd.read_table(output_file_path, sep='\s+')
@@ -83,12 +107,14 @@ class surface:
         self.hydrosphere.molarity['Na'] = solution_df.at[1, 'Na']
         self.hydrosphere.molarity['Cl'] = solution_df.at[1, 'Cl']
 
-        if not keep_atmosphere_constant:
+        d_CO2 = solution_df.at[1, 'd_CO2(g)']
 
-            d_CO2 = solution_df.at[1, 'd_CO2(g)']
+        if not keep_atmosphere_constant:
 
             mol_CO2_new = mol_CO2 * (1 + d_CO2)
             x_CO2_new = mol_CO2_new / mol_atm
 
             self.atmosphere.change_gas_species('CO2', x_CO2_new)
+
+        return d_CO2
 
