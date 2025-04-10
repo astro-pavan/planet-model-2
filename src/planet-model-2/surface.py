f39d29d8d02bd6dd95a8910984be64b5765dabe2
astro-pavan <pt426@cam.ac.uk>
2025-04-10 14:29:57 +0100

    Can do gas phase equilbrium and water evaporation and modify the hydrosphere and atmosphere.

--- src/planet-model-2/surface.py
+++ src/planet-model-2/surface.py
@@ -5,6 +5,7 @@ from utils import modify_file_by_lines
 
 import numpy as np
 import os
+import pandas as pd
 
 PHREEQC_path = '/data/pt426/phreeqc/bin'
 
@@ -22,55 +23,72 @@ class surface:
 
         relative_humidity = 0.8
 
-        P_H2O = relative_humidity * saturation_vapour_pressure(self.T)
+        Tc = self.T + ABSOLUTE_ZERO # Convert to Celsius
 
-        print(P_H2O / 1e2)
+        # from Huang (2018)
+        P_sat_vap_water = np.exp(34.494 - (4924.99/(Tc + 237.1))) / ((Tc + 105) ** 1.57) # in Pa
+        P_sat_vap_ice = np.exp(43.494 - (6545.8/(Tc + 278))) / ((Tc + 868) ** 2) # in Pa
+
+        saturation_vapour_pressure = P_sat_vap_water if Tc > 0 else P_sat_vap_ice
+
+        P_H2O = relative_humidity * saturation_vapour_pressure
         
         x_H2O_old = self.atmosphere.x_gas['H2O']
         x_H2O_new = np.full_like(x_H2O_old, P_H2O / self.P)
 
         self.atmosphere.change_gas_species('H2O', x_H2O_new)
 
-        print(self.atmosphere.x_gas)
-
-    def gas_ocean_interaction(self):
-
-        pass
-
-
-def saturation_vapour_pressure(T):
+    def phreeqc_equilibrium_phase(self, keep_atmosphere_constant=False):
     
-    Tc = T + ABSOLUTE_ZERO # Convert to Celsius
+        input_template_file_path = 'templates/phreeqc_co2_equilibrium_input_template.txt'
+        input_file_path_new = f'{PHREEQC_path}/input'
+        output_file_path = f'{PHREEQC_path}/output.txt'
 
-    # from Huang (2018)
-    P_sat_vap_water = np.exp(34.494 - (4924.99/(Tc + 237.1))) / ((Tc + 105) ** 1.57) # in Pa
-    P_sat_vap_ice = np.exp(43.494 - (6545.8/(Tc + 278))) / ((Tc + 868) ** 2) # in Pa
+        atmosphere_column_density = self.atmosphere.column_density()
+        hydrosphere_column_density = self.hydrosphere.column_density()
 
-    return np.where(Tc > 0, P_sat_vap_water, P_sat_vap_ice)
+        mmw_atmosphere = self.atmosphere.mmw[0]
 
+        mol_atm = atmosphere_column_density / mmw_atmosphere
+        mol_CO2 = mol_atm * self.atmosphere.x_gas['CO2'][0]
 
+        scale_factor = 1 / mol_CO2
+        m_water = hydrosphere_column_density * scale_factor
 
-def phreeqc_CO2_equilibrium_phase(P, T, pH, C_dissolved, P_CO2, m_water=1, mol_CO2=10):
-    
-    input_template_file_path = 'templates/phreeqc_co2_equilibrium_input_template.txt'
-    input_file_path_new = f'{PHREEQC_path}/input'
-    output_file_path = f'{PHREEQC_path}/input.out'
-    
-    input_file_modifications = {
-            4 : f'    temp        {T + ABSOLUTE_ZERO:.1f}        # Temperature in degrees Celsius',
-            5 : f'    pressure    {P / EARTH_ATM:.2f}         # Pressure in atmospheres',
-            6 : f'    pH          {pH:.1f}         # Initial pH',
-            11 : f'    C           {C_dissolved:.2f}         # Total dissolved carbon',
-            12 : f'    water       {m_water:.2f}         # mass of water in kg',
-            15 : f'    CO2(g)      {np.log10(P_CO2 / EARTH_ATM)}        {mol_CO2:.2f}    # partial pressure in log(atm) and number of moles of CO2',
-        }
+        P_CO2 = self.P * self.atmosphere.x_gas['CO2'][0]
+        
+        input_file_modifications = {
+                4 : f'    temp        {self.T + ABSOLUTE_ZERO:.1f}        # Temperature in degrees Celsius',
+                5 : f'    pressure    {self.P / EARTH_ATM:.2f}         # Pressure in atmospheres',
+                6 : f'    pH          {self.hydrosphere.pH:.1f}         # Initial pH',
+                8 : f'    Ca          {self.hydrosphere.molarity["Ca"]:.2f}         # Calcium concentration',
+                9 : f'    Na          {self.hydrosphere.molarity["Na"]:.2f}         # Sodium concentration',
+                10 : f'    Cl          {self.hydrosphere.molarity["Cl"]:.2f}         # Chloride concentration',
+                11 : f'    C           {self.hydrosphere.molarity["C"]:.2f}         # Total dissolved carbon',
+                12 : f'    water       {m_water:.2f}         # mass of water in kg',
+                15 : f'    CO2(g)      {np.log10(P_CO2 / EARTH_ATM)}        {1:.2f}    # partial pressure in log(atm) and number of moles of CO2',
+            }
 
-    modify_file_by_lines(input_template_file_path, input_file_path_new, input_file_modifications)
+        modify_file_by_lines(input_template_file_path, input_file_path_new, input_file_modifications)
 
-    wd = os.getcwd()
-    os.chdir(PHREEQC_path)
-    os.system('./phreeqc input')
-    os.chdir(wd)
+        wd = os.getcwd()
+        os.chdir(PHREEQC_path)
+        os.system('./phreeqc input')
+        os.chdir(wd)
 
+        solution_df = pd.read_table(output_file_path, sep='\s+')
 
-    
+        self.hydrosphere.molarity['C'] = solution_df.at[1, 'C']
+        self.hydrosphere.molarity['Ca'] = solution_df.at[1, 'Ca']
+        self.hydrosphere.molarity['Na'] = solution_df.at[1, 'Na']
+        self.hydrosphere.molarity['Cl'] = solution_df.at[1, 'Cl']
+
+        if not keep_atmosphere_constant:
+
+            d_CO2 = solution_df.at[1, 'd_CO2(g)']
+
+            mol_CO2_new = mol_CO2 * (1 + d_CO2)
+            x_CO2_new = mol_CO2_new / mol_atm
+
+            self.atmosphere.change_gas_species('CO2', x_CO2_new)
+
