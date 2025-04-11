32793c82c5f608783524568e77658cbbadef0b87
astro-pavan <pt426@cam.ac.uk>
2025-04-11 15:04:44 +0100

    Function added to save planet as HDF5

--- src/planet-model-2/planet.py
+++ src/planet-model-2/planet.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 import subprocess
+import h5py
 
 from layers.core import core
 from layers.hydrosphere import hydrosphere
@@ -16,17 +17,14 @@ magrathea_path = '/data/pt426/Magrathea'
 
 class planet:
 
-    def __init__(self, M, R, F_star, P_surface, T_initial, spec_type, atm_vmrs):
+    def __init__(self, M, R, F_star, P_surface, T_initial, spec_type, atm_vmrs, tidally_locked=True):
         
         self.mass = M
         self.radius = R
 
-        self.instellation = F_star
-        self.host_star_spectral_type = spec_type
-
         print('GENERATING ATMOSPHERE...')
 
-        self.atmosphere = atmosphere(self.radius, self.mass, atm_vmrs, self.instellation, spec_type, P_surface, T_initial)
+        self.atmosphere = atmosphere(self.radius, self.mass, atm_vmrs, F_star, spec_type, P_surface, T_initial, tidally_locked)
 
         print('ATMOSPHERE GENERATED')
 
@@ -34,17 +32,17 @@ class planet:
 
         df_core, df_hydro = self.run_Magarathea(P_surface, self.atmosphere.T[-1])
 
-        m_hydro = np.array(df_hydro['M (earth)']) * M_EARTH
-        r_hydro = np.array(df_hydro['Radius (earth)']) * R_EARTH
-        P_hydro = np.array(df_hydro['P (GPa)']) * 1e9
-        T_hydro = np.array(df_hydro['T (K)'])
-        rho_hydro =  np.array(df_hydro['Density (g cm^-3)']) * 1e3
+        m_hydro = np.array(df_hydro['M (earth)'])[::-1] * M_EARTH
+        r_hydro = np.array(df_hydro['Radius (earth)'])[::-1] * R_EARTH
+        P_hydro = np.array(df_hydro['P (GPa)'])[::-1] * 1e9
+        T_hydro = np.array(df_hydro['T (K)'])[::-1]
+        rho_hydro =  np.array(df_hydro['Density (g cm^-3)'])[::-1] * 1e3
 
-        m_core = np.array(df_core['M (earth)']) * M_EARTH
-        r_core = np.array(df_core['Radius (earth)']) * R_EARTH
-        P_core = np.array(df_core['P (GPa)']) * 1e9
-        T_core = np.array(df_core['T (K)'])  
-        rho_core =  np.array(df_core['Density (g cm^-3)']) * 1e3
+        m_core = np.array(df_core['M (earth)'])[::-1] * M_EARTH
+        r_core = np.array(df_core['Radius (earth)'])[::-1] * R_EARTH
+        P_core = np.array(df_core['P (GPa)'])[::-1] * 1e9
+        T_core = np.array(df_core['T (K)'])[::-1]  
+        rho_core =  np.array(df_core['Density (g cm^-3)'])[::-1] * 1e3
 
         self.hydrosphere = hydrosphere(m_hydro, r_hydro, P_hydro, T_hydro, rho_hydro)
         self.core = core(m_core, r_core, P_core, T_core, rho_core)
@@ -52,8 +50,6 @@ class planet:
         print('INTERNAL STRUCTURE GENERATED')
 
         self.surface = surface(self.hydrosphere, self.atmosphere)
-        
-        
 
 
     def run_Magarathea(self, P_surface, T_surface):
@@ -83,7 +79,7 @@ class planet:
         modify_file_by_lines(mode4_config_file_path, mode4_config_file_path, mode4_config_file_modifications)
 
         os.chdir(magrathea_path)
-        subprocess.run(['./planet', 'run/mode4.cfg'])
+        subprocess.run(['./planet', 'run/mode4.cfg'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
         mode4_results_table = pd.read_table(mode4_output_file_path, sep='\s+')
 
@@ -101,7 +97,7 @@ class planet:
 
         modify_file_by_lines(mode0_config_file_path, mode0_config_file_path, mode0_config_file_modifications)
 
-        subprocess.run(['./planet', 'run/mode0.cfg'])
+        subprocess.run(['./planet', 'run/mode0.cfg'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         os.chdir(wd)
 
         df = pd.read_table(mode0_output_file_path, sep='	 ', engine='python')
@@ -133,10 +129,54 @@ class planet:
         plt.savefig('PT_ocean.png')
         plt.close()
 
+    def save_to_hdf5(self, filename):
+        
+        with h5py.File(filename, 'w') as f:
+            
+            f.attrs['Mass'] = self.mass
+            f.attrs['Radius'] = self.radius
+
+            f.attrs['Instellation'] = self.atmosphere.instellation
+            f.attrs['Host Star Spectral Type'] = self.atmosphere.host_star_spectral_type
+
+            core_grp = f.create_group('Core')
+            hydro_grp = f.create_group('Hydrosphere')
+            atm_grp = f.create_group('Atmosphere')
+
+            core_grp.create_dataset('m', data=self.core.m)
+            core_grp.create_dataset('r', data=self.core.r)
+            core_grp.create_dataset('P', data=self.core.P)
+            core_grp.create_dataset('T', data=self.core.T)
+            core_grp.create_dataset('rho', data=self.core.rho)
+
+            hydro_grp.create_dataset('m', data=self.hydrosphere.m)
+            hydro_grp.create_dataset('r', data=self.hydrosphere.r)
+            hydro_grp.create_dataset('P', data=self.hydrosphere.P)
+            hydro_grp.create_dataset('T', data=self.hydrosphere.T)
+            hydro_grp.create_dataset('rho', data=self.hydrosphere.rho)
+
+            molarity_grp = hydro_grp.create_group('Molarity')
+            for species in self.hydrosphere.molarity.keys():
+                molarity_grp.attrs[species] = self.hydrosphere.molarity[species]
+
+            atm_grp.create_dataset('m', data=self.atmosphere.m)
+            atm_grp.create_dataset('r', data=self.atmosphere.r)
+            atm_grp.create_dataset('P', data=self.atmosphere.P)
+            atm_grp.create_dataset('T', data=self.atmosphere.T)
+            atm_grp.create_dataset('rho', data=self.atmosphere.rho)
+            atm_grp.create_dataset('mmw', data=self.atmosphere.mmw)
+            
+            x_gas_grp = atm_grp.create_group('x_gas')
+            for gas in self.atmosphere.x_gas.keys():
+                x_gas_grp.create_dataset(gas, self.atmosphere.x_gas[gas])
+
+
 
 
 if __name__ == '__main__':
 
-    test_planet = planet(1 * M_EARTH, 1 * R_EARTH, 0.5, 1e5, 270, 'G2', {'N2' : [0.999], 'CO2' : [0.001], 'H2O' : [0.00]})
+    test_planet = planet(1 * M_EARTH, 1 * R_EARTH, 1, 1e5, 270, 'G2', 
+                         {'N2' : [0.78], 'O2' : [0.21], 'Ar' : [0.009], 'CO2' : [0.0004], 'H2O' : [0.00]},
+                           tidally_locked=False)
 
         
