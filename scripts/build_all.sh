31944bca3665ce3da04021ed1f90b81666a85ef5
astro-pavan <pt426@cam.ac.uk>
2025-05-14 17:28:22 +0100

    Made installation script

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -1,2 +1,23 @@
 #!/bin/bash
 
+
+# installs Magrathea
+cd external/Magrathea
+make -B
+cd ../..
+
+# makes installation directory for PHREEQC
+cd external
+mkdir phreeqc
+cd phreeqc
+INSTALLDIR=$PWD
+cd ..
+
+# unpacks and installs PHREEQC
+tar -xvzf phreeqc-3.8.6-17100.tar.gz
+cd phreeqc-3.8.6-17100
+mkdir Release
+cd Release
+../configure --prefix=$INSTALLDIR
+make
+make install
